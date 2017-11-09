6a95aa027a9d2c7c6d4e3160da2bb7752fc5a0d4
AhmadZuhriyani <izuhryan@gmail.com>
2017-11-09 09:50:09 +0800

    Memasukan Value INT dan Perintah CIN Cout

--- Untitled1.cpp
+++ Untitled1.cpp
@@ -2,6 +2,15 @@
 using namespace std;
 
 int main(){
+	
+	int gp,jk;
+	int lmbr,gs,pjk,gk;
+	
+	cout <<"Penghitungan Gaji Karyawan"<<endl;
+	cout <<"Masukkan Gaji Pokok Anda : Rp. ";
+	cin >>gp;
+	cout <<"Masukkan Jam Kerja Anda : ";
+	cin>>jk;
 }
 	
 
