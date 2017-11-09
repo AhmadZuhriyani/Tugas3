8d755115c8cca2161d207cf925fbee7bb78b14c2
AhmadZuhriyani <izuhryan@gmail.com>
2017-11-09 09:43:04 +0800

    Memasukan Cin dan Cout
    2

--- Untitled1.cpp
+++ Untitled1.cpp
@@ -3,4 +3,11 @@
 using namespace std;
 
 int main (){
+	int x,y,z;
+	
+	cout <<"Menghitung Selisih Absolute"<<endl;
+	cout<<"Masukkan Nilai X : ";
+	cin>>x;
+	cout <<"Masukkan Nilai Y : ";
+	cin>>y;
 }
