19fcf4aa2ac319f371c4cf8df21e75e85c59d796
AhmadZuhriyani <izuhryan@gmail.com>
2017-11-09 09:53:08 +0800

    Menambahkan Perintah Benar atau Salah utk COUT

--- Untitled1.cpp
+++ Untitled1.cpp
@@ -5,6 +5,15 @@ int main(){
 	int x;
 	cout << "Masukkan Nilai : ";
 	cin >>x;
+	if (x>0){
+		cout <<"Nilai Positif";
+	}
+	else if (x<0){
+		cout <<"Nilai Negatif";
+	}
+	else{
+		cout <<"Nilai Nol";
+	}
 	
 }
 	
