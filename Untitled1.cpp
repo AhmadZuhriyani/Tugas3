7efedcda4937c673f7c14d563327675536e8792b
AhmadZuhriyani <izuhryan@gmail.com>
2017-11-09 09:53:26 +0800

    Membuat Tugas5

--- Untitled1.cpp
+++ Untitled1.cpp
@@ -2,18 +2,6 @@
 using namespace std;
 
 int main(){
-	int x;
-	cout << "Masukkan Nilai : ";
-	cin >>x;
-	if (x>0){
-		cout <<"Nilai Positif";
-	}
-	else if (x<0){
-		cout <<"Nilai Negatif";
-	}
-	else{
-		cout <<"Nilai Nol";
-	}
 	
 }
 	
