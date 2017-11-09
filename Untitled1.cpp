0cbda3a16df5a73cdc03159434c663e6cc1eee75
AhmadZuhriyani <izuhryan@gmail.com>
2017-11-09 09:45:15 +0800

    Membuat Tugas2
    4

--- Untitled1.cpp
+++ Untitled1.cpp
@@ -2,20 +2,4 @@
 using namespace std;
 
 int main (){
-	int x,y,z;
-	
-	cout <<"Menghitung Selisih Absolute"<<endl;
-	cout<<"Masukkan Nilai X : ";
-	cin>>x;
-	cout <<"Masukkan Nilai Y : ";
-	cin>>y;
-	
-		if (x>y){
-		z=x-y;
-	}
-	else if (y>x){
-		z=y-x;
-	}
-	
-	cout <<"Hasil : "<<z;
 }
