f0c175bfa0a8ae866ad8c76a2bc03bb5c72a75cd
AhmadZuhriyani <izuhryan@gmail.com>
2017-11-09 09:47:00 +0800

    Memasukan NILAI INT dan CIN dan COUT
    5

--- Untitled1.cpp
+++ Untitled1.cpp
@@ -1,5 +1,12 @@
 #include <iostream>
 using namespace std;
+ 
+int main(int argc, char** argv){
+	int pjg,lbr,l,pil,a,t;
+	
+	cout <<"1. Persegi Panjang\n";
+	cout <<"2. Segitiga\n";
+	cout <<"Masukkan Pilihan : ";
+	cin >>pil;
+	
 
-int main (){
-}
